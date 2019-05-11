d8d222ba42fd318b6be2c58ad653e7efd857cdf6
azharizkita <azharizkita@outlook.com>
2019-05-11 13:57:54 +0700

    Update welcome page menu

--- application/views/content/welcome_message.php
+++ application/views/content/welcome_message.php
@@ -1,3 +1,7 @@
 <div class="container" style="padding-top: 10px">
     <a href="<?php echo base_url()?>home_regist">Register</a>
+    <a href="<?php echo base_url()?>cadmin">Contact (Admin)</a>
+    <a href="<?php echo base_url()?>ccontact">Contact (User)</a>
+    <a href="<?php echo base_url()?>distribusi">Distribusi</a>
+    <a href="<?php echo base_url()?>transaksi">Transaksi</a>
 </div>
