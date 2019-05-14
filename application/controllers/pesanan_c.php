7fdc1bedfed8051fcc54c3bb42385beb5299c333
muhammadmagrif <36444159+muhammadmagrif@users.noreply.github.com>
2019-05-14 11:30:09 +0700

    Add files via upload

--- application/controllers/pesanan_c.php
+++ application/controllers/pesanan_c.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class pesanan_c extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,8 +18,20 @@ class Welcome extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('pesanan_m');
+        $this->load->helper('url');
+        $this->load->helper('form');
+    }
 	public function index()
 	{
-		$this->load->view('welcome_message');
-	}
+        $this->load->view('pesan_v');
+    }
+
+    public function pesan(){
+        $this->pesanan_m->insPes();
+    }
+
 }
